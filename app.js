8c08c31bf8a7a255eb25fbfa2655fb448909cbf4
varinderpalsingh <45623099+varinderpalsingh@users.noreply.github.com>
2020-12-28 00:27:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.lot('hello world 2");
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(3200);
